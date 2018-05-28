ca60145bcf1cb242e8f92c1963c65174abcb6da2
Papa NDIAYE <papa.ndiaye@rte-france.com>
2018-05-28 17:23:45 +0200

    initial commit

---
+++ main.cpp
@@ -0,0 +1,67 @@
+#include <ctype.h>
+#include <stdio.h>     /* for printf */
+#include <stdlib.h>    /* for exit */
+#include <iostream>
+#include <getopt.h>
+
+int
+main(int argc, char **argv)
+{
+	
+    int c;
+	
+   while (1) {
+        int this_option_optind = optind ? optind : 1;
+        int option_index = 0;
+        static struct option long_options[] = {
+            {"hourly",  required_argument, 0, 'c'},
+            {"file",    required_argument, 0,  0 },
+            {0,         0,                 0,  0 }
+        };
+
+       c = getopt_long(argc, argv, "abc:d:012",
+                 long_options, &option_index);
+        if (c == -1)
+            break;
+
+       switch (c) {
+        case 0:
+            printf("option %s", long_options[option_index].name);
+            if (optarg)
+                printf(" with arg %s", optarg);
+            printf("\n");
+            break;
+
+       case 'o':
+            printf("option open\n");
+            break;
+
+       case 'c':
+            printf("option close\n");
+            break;
+
+       case 'e':
+            printf("option e with value '%s'\n", optarg);
+            break;
+
+       case 'd':
+            printf("option d with value '%s'\n", optarg);
+            break;
+
+       case '?':
+            break;
+
+       default:
+            printf("?? getopt returned character code 0%o ??\n", c);
+        }
+    }
+
+   if (optind < argc) {
+        printf("non-option ARGV-elements: ");
+        while (optind < argc)
+            printf("%s ", argv[optind++]);
+        printf("\n");
+    }
+
+   exit(EXIT_SUCCESS);
+}
